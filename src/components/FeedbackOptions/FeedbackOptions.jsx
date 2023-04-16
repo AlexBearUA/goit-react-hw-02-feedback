b19865acc250d2a0e00c2dc1815c9f8931c890a6
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-04-16 22:08:56 +0200

    +1

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
+
 import css from './FeedbackOptions.module.css';
 export const FeedbackOptions = ({ onLeaveFeedback, options }) => (
   <ul className={css.FeedbackOptions}>
     {options.map(option => (
-      <li key={nanoid()}>
+      <li key={option}>
         <button type="button" data-value={option} onClick={onLeaveFeedback}>
           {option[0].toUpperCase() + option.slice(1)}
         </button>
